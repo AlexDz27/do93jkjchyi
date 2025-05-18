5ee812182cc5e89be5597d2ba57bfedad8e7b8db
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-05-18 14:05:35 +0300

    w

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
-// const theCard = document.getElementById('theCard')
-// console.log(Number(theCard.dataset.id))
 // TODO: also make POC of dumb tg posts and uploading data via them?
 
+window.addEventListener('popstate', (e) => {
+  // console.log('nav occured')
+  // console.log(e.state)
 
-const sneakers = document.getElementById('main').children
+  main.classList.add('main--active')
+  section.classList.remove('section--active')
+})
+
+const main = document.getElementById('main')
+const section = document.getElementById('section')
+
+const sneakers = main.children
 for (const sneaker of sneakers) {
   sneaker.onclick = () => {
-    const id = Number(sneaker.dataset.id)
-    const datum = data[id - 1]
-    console.log(datum)
+    // const id = Number(sneaker.dataset.id)
+    // const datum = data[id - 1]
+    // console.log(datum)
+
+    main.classList.remove('main--active')
+    section.classList.add('section--active')
+    window.history.pushState(
+      {page: 'section'},
+      'Sneaker',
+      '/sneaker'
+    )
+    // console.log(history.length)
+    // console.log(history)
   }
 }
