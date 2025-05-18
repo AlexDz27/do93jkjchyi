9b5d21b989ad1b096d53fc0615b2e8b2b7b406fe
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-05-18 12:25:33 +0300

    w

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-
-
+// const theCard = document.getElementById('theCard')
+// console.log(Number(theCard.dataset.id))
 // TODO: also make POC of dumb tg posts and uploading data via them?
+
+
+const sneakers = document.getElementById('main').children
+for (const sneaker of sneakers) {
+  sneaker.onclick = () => {
+    const id = Number(sneaker.dataset.id)
+    const datum = data[id - 1]
+    console.log(datum)
+  }
+}
