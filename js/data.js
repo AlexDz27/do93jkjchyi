5c717010e7d2864a6a1e884e823ed16aa7c88d31
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-05-18 12:25:47 +0300

    w, no-edit

---
+++ js/data.js
@@ -0,0 +1,78 @@
+const data = [
+  {
+    id: 1,
+    name: "New Balance 530 'White Silver Blue'",
+  },
+  {
+    id: 2,
+    name: "Nike Air Max Plus 'Summit' White Pink",
+  },
+  {
+    id: 3,
+    name: "New Balance 327 'Phantom Black'",
+  },
+  {
+    id: 4,
+    name: "New Balance 327 'Grey Matter'",
+  },
+  {
+    id: 5,
+    name: "Jordan Jumpman Jack TR Travis Scott 'Sail'",
+  },
+  {
+    id: 6,
+    name: "Vans Old Skool Comfy Cush 'Black'",
+  },
+  {
+    id: 7,
+    name: "Vans Old Skool x Imran Potato 'Black White'",
+  },
+  {
+    id: 8,
+    name: "Nike Air Max 97 'Neon Mirage'",
+  },
+  {
+    id: 9,
+    name: "Nike Air Force 1 'Barely Rose'",
+  },
+  {
+    id: 10,
+    name: "Nike Air Max Plus 'Solar Flare'",
+  },
+  {
+    id: 11,
+    name: "Jordan 4 Retro 'Desert Clay'",
+  },
+  {
+    id: 12,
+    name: "Jordan 3 Retro 'Smoke Grey Suede'",
+  },
+  {
+    id: 13,
+    name: "New Balance 327 'Grey Matter'",
+  },
+  {
+    id: 14,
+    name: "Vans Slip-On 'Pebble Grey'",
+  },
+  {
+    id: 15,
+    name: "Adidas Gazelle 'Pebble Pink'",
+  },
+  {
+    id: 16,
+    name: "Adidas Stan Smith 'Pale Moss'",
+  },
+  {
+    id: 17,
+    name: "Nike Air Max 90 'Terracotta Dawn'",
+  },
+  {
+    id: 18,
+    name: "Jordan 1 Low 'Antique Bone'",
+  },
+  {
+    id: 19,
+    name: "Vans Old Skool x Something 'Concrete Waves'",
+  },
+]
